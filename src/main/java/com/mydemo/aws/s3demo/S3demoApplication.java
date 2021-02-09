34abae172bb14e49607fb48120ed5d72484e9a7a
sahulkodali <sahulkodali@icloud.com>
2021-02-08 20:08:01 -0500

    initial commit

---
+++ src/main/java/com/mydemo/aws/s3demo/S3demoApplication.java
@@ -0,0 +1,13 @@
+package com.mydemo.aws.s3demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3demoApplication.class, args);
+	}
+
+}
